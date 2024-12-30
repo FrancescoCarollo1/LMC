b4b7d5722d484fdc63692b917bb2b850e71c1eee
Francesco Carollo <carollofrancesco2@gmail.com>
2024-12-30 17:07:14 +0100

    added TODO file and function to parse file

--- Assembler.py
+++ Assembler.py
@@ -1,21 +1,30 @@
 #SM3201419 Francesco Carollo
 # This is the Assembler.py file. It contains the Assembler class, which is used to assemble the LMC code into machine code.
-import LMC
 
-class Assembler:
+'''
+class Assembler():
     def __init__(self):
         self.instructions = {
-            "ADD": LMC.add,
-            "SUB": LMC.sub,
-            "STA": LMC.write_memory,
-            "LDA": LMC.accum_read_in,
-            "BRA": LMC.branch,
-            "BRZ": LMC.branch_zero,
-            "BRP": LMC.branch_positive,
-            "INP": LMC.accum_read_in,
-            "OUT": LMC.accum_add_out,
-            "HLT": LMC.halt,
-            "DAT": self.dat
+            "ADD",
+            "SUB",
+            "STA",
+            "LDA",
+            "BRA",
+            "BRZ",
+            "BRP",
+            "INP",
+            "OUT",
+            "HLT",
+            "DAT",
             }
-    
+'''    
+with open('test_programs/looping.lmc') as f:
+    lines = []
+    for line in f.readlines():
+        line = line.split("//")[0].strip("\n ").split()
+        line = [token.upper() for token in line]
+        if line:
+            lines.append(line)
+    print(lines)
+
     
