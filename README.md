1e48a53fa71a3a905f57b7794ceede35130bb329
Francesco Carollo <carollofrancesco2@gmail.com>
2024-12-31 17:33:40 +0100

    tested all the programs with different inputs

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # LMC
+
+Francesco Conti è Mooooolto Bravo
+
 progetto in python per il corso di Programmazione avanzata e parallela
+
+
+INPUT USATI PER FARE I TEST:
+Counting: [10]
+Exec: [901, 902, 705, 600, 0, 4, 5, 6, 7, 8, 9, 0]
+Looping non richiede input.
+Multiplication: [6, 7]
+Quine non richiede input.
+Reverse: [5, 10, 15, 0]
+Squares: [1, 2, 3, 4, 5, 0]
--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import Assembler
 
 def main():
     assembler = Assembler.Assembler()
-    assembler.assemble("test_programs/counting.lmc")
+    assembler.assemble("test_programs/squares.lmc")
     machine_code = assembler.memory
     lmc = LMC.LMC()
-    #lmc.input = [901, 902, 705, 600, 0, 4, 5, 6, 7, 8, 9, 0]
-    lmc.input = [42]
+    lmc.input = [1, 2, 3, 4, 5, 0]
     lmc.load_memory(machine_code)
     lmc.run()
+    print(lmc.output)
+    print(len(lmc.output))
 
 if __name__ == "__main__":
     main()
